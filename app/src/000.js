f05e3f8f9c10bdc5b007fd3aa689ff76c6883d9a
gajendrakumarps <gajendra.srinivas@gmail.com>
2022-06-11 18:56:10 +0530

    Update 000.js
    correcting code to give environment variable in the response object

--- app/src/000.js
+++ app/src/000.js
@@ -41,10 +41,11 @@ exec('bin/005 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
 });
 
 app.get('/secret_word', function (req,res) {
-const { exec } = require('child_process');
-exec('bin/006 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
-  return res.send(`${stdout}`);
-});
+  const name = process.env.SECRET_WORD;
+  return res.send({name});
 });
 
-app.listen(port, () => console.log(`Rearc quest listening on port ${port}!`))
+app.get('/secret', function (req,res) {
+  return res.send(`Mynamesecret`);
+});
+app.listen(port, "0.0.0.0")
